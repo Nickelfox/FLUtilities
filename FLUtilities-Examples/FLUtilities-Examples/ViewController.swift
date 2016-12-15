339ff5fe52c7aa972486d687a3c6319698e15eda
Ravindra Soni <soni@nickelfox.com>
2016-12-15 19:36:25 +0530

    feat: Initial Commit

---
+++ FLUtilities-Examples/FLUtilities-Examples/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FLUtilities-Examples
+//
+//  Created by Ravindra Soni on 15/12/16.
+//  Copyright © 2016 Nickelfox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		self.demo()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
